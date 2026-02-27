semgrep/semgrep

Simplify Target.t, remove the DependencySource case (semgrep/semgrep-proprietary#3613)
test plan:
make test

synced from Pro 549586f2ef2cec1da39e9efc22d57f26e0c4c09d

src/target/Target.ml
@@ -26,7 +26,7 @@ module Out = Semgrep_output_v1_j
 type path = { origin : Origin.t; internal_path_to_content : Fpath.t }
 [@@deriving show, eq]
 
-type regular = {
+type t = {
   path : path;
   analyzer : Analyzer.t;
   products : Product.t list;
@@ -34,26 +34,14 @@ type regular = {
 }
 [@@deriving show]
 
-type t = Regular of regular | DependencySource of Dependency_source.t
-[@@deriving show]
-
 (*****************************************************************************)
 (* Dumpers *)
 (*****************************************************************************)
 
-let pp_debug_dependency_source f (t : Dependency_source.t) =
-  Format.fprintf f "%s" (Dependency_source.show t)
-
-let pp_debug_regular f (t : regular) =
-  Format.fprintf f "%s (%s)"
+let pp_debug f (t : t) =
+  Format.fprintf f "target file: %s (%s)"
     (t.path.internal_path_to_content |> Fpath.to_string)
     (t.analyzer |> Analyzer.to_string)
-
-let pp_debug f = function
-  | Regular t -> Format.fprintf f "target file: %a" pp_debug_regular t
-  | DependencySource t ->
-      Format.fprintf f "target dependency source: %a" pp_debug_dependency_source
-        t
 
 (* needed because of some deriving yojson in Targeting_stat.ml *)
 let to_yojson (x : t) : Yojson.Safe.t =
@@ -93,72 +81,53 @@ let path_of_origin (origin : Origin.t) : path =
 (* Builders *)
 (*****************************************************************************)
 
-let mk_regular ?dependency_source analyzer products (origin : Origin.t) :
-    regular =
+let mk_target_origin ?dependency_source analyzer products (origin : Origin.t) :
+    t =
   { path = path_of_origin origin; analyzer; products; dependency_source }
 
 (* useful in test context *)
-let mk_target (analyzer : Analyzer.t) (file : Fpath.t) : t =
+let mk_target_fpath (analyzer : Analyzer.t) (file : Fpath.t) : t =
   let all = Product.all in
   (* TODO: should do the check in the other mk_xxx ? *)
   assert (UFile.is_reg ~follow_symlinks:true file);
-  Regular (mk_regular analyzer all (Origin.File file))
+  mk_target_origin analyzer all (Origin.File file)
 
 (* useful in test context or DeepScan context *)
-let mk_lang_target (lang : Lang.t) (file : Fpath.t) : regular =
-  mk_regular (Analyzer.of_lang lang) Product.all (File file)
+let mk_lang_target (lang : Lang.t) (file : Fpath.t) : t =
+  mk_target_fpath (Analyzer.of_lang lang) file
 
 (*****************************************************************************)
 (* Semgrep_output_v1.target -> Target.t *)
 (*****************************************************************************)
 (* old: used to be Input_to_core.target -> Target.t *)
 
-let code_target_location_of_input_to_core
-    ({ path; analyzer; products; dependency_source } : Out.code_target) :
-    regular =
-  mk_regular ?dependency_source analyzer products (File path)
+let target_location_of_input_to_core
+    ({ path; analyzer; products; dependency_source } : Out.code_target) : t =
+  mk_target_origin ?dependency_source analyzer products (File path)
 
 let target_of_target (input : Out.target) : t =
   match input with
-  | `CodeTarget x -> Regular (code_target_location_of_input_to_core x)
-  | `DependencySourceTarget x -> DependencySource x
+  | `CodeTarget x -> target_location_of_input_to_core x
+  | `DependencySourceTarget _x ->
+      failwith
+        "Impossible: pysemgrep should not generate DependencySourceTarget"
 
 (*****************************************************************************)
 (* Accessors *)
 (*****************************************************************************)
 
-let rec internal_path_of_dependency_source (source : Dependency_source.t) :
-    Fpath.t =
-  match source with
-  | ManifestOnly { path; _ } -> path
-  | LockfileOnly { path; _ } -> path
-  | ManifestLockfile (_, { path; _ }) -> path
-  (* TODO: this seems really dubious, what should we do here? *)
-  | MultiLockfile (src :: _) -> internal_path_of_dependency_source src
-  | MultiLockfile [] -> failwith "MultiLockfile is empty"
-
 let internal_path (target : t) : Fpath.t =
-  match target with
-  | Regular { path = { internal_path_to_content; _ }; _ } ->
-      internal_path_to_content
-  | DependencySource src -> internal_path_of_dependency_source src
+  let { path = { internal_path_to_content; _ }; _ } = target in
+  internal_path_to_content
 
 let origin (target : t) : Origin.t =
-  match target with
-  | Regular { path = { origin; _ }; _ } -> origin
-  | DependencySource src -> Origin.File (internal_path_of_dependency_source src)
-
-let analyzer (target : t) : Analyzer.t option =
-  match target with
-  | Regular r -> Some r.analyzer
-  | DependencySource _ -> None
+  let { path = { origin; _ }; _ } = target in
+  origin
 
 let analyzers_of_targets (targets : t list) : Analyzer.t Set_.t =
   List.fold_left
     (fun set target ->
-      match analyzer target with
-      | None -> set
-      | Some a ->
-          let analyzers = Analyzer.flatten a in
-          List_.fold_right Set_.add analyzers set)
+      let a = target.analyzer in
+      let analyzers = Analyzer.flatten a in
+      List_.fold_right Set_.add analyzers set)
     Set_.empty targets

src/target/Target.mli
@@ -32,10 +32,23 @@ type path = {
   } *)
 
 (*****************************************************************************)
-(* Regular target *)
+(* Main type *)
 (*****************************************************************************)
 
-type regular = {
+(*  A semgrep target comprising source code (or, for regex/generic, arbitrary
+    text data) to be executed. This contains all of the details needed to be
+    able to determine how to scan a target, e.g.,
+
+    {ul
+      {- What products should we select rules from?}
+      {- Where can we get the contents of the target?}
+      {- What language should we analyze the target as?}
+    }
+
+    However, it does not contain the actual contents (parsed or otherwise) of
+    the target itself. For that, see {!Xtarget.t}.
+*)
+type t = {
   path : path;
   analyzer : Analyzer.t;  (** The analyzer to use when scanning this target. *)
   products : Product.t list;
@@ -48,42 +61,22 @@ type regular = {
           specified by the given source. Core doesn't need to worry about
           determining these associations; rather, the target selection and
           generation process must resolve these connections as part of
-          generating regular targets. *)
+          generating targets. *)
 }
-(** A regular semgrep target, comprising source code (or, for
-   regex/generic, arbitrary text data) to be executed. See also {!Xtarget.t},
-   an augmented version which also has the contents. *)
-
-(*****************************************************************************)
-(* Main type *)
-(*****************************************************************************)
-
-(** A Semgrep target. This contains all of the details needed to be able to
-    determine how to scan a target, e.g.,
-
-    {ul
-      {- What products should we select rules from?}
-      {- Where can we get the contents of the target?}
-      {- What language should we analyze the target as?}
-    }
-
-    However, it does not contain the actual contents (parsed or otherwise) of
-    the target itself. For that, see {!Xtarget.t} or {!Lockfile_xtarget}.
- *)
-type t = Regular of regular | DependencySource of Dependency_source.t
 [@@deriving show]
 
 (*****************************************************************************)
 (* Builders *)
 (*****************************************************************************)
 
-val mk_regular :
+(* TODO? make default ?product=Product.all? *)
+val mk_target_origin :
   ?dependency_source:Dependency_source.t ->
   Analyzer.t ->
   Product.t list ->
   Origin.t ->
-  regular
-(** [mk_regular analyzer products origin] is a {!regular} target
+  t
+(** [mk_target analyzer products origin] is a target
       originating from [origin] to be analyzed with [analyzer] for [products].
       If [dependency_source] is specified then it shall be used as the associated
       dependency source if dependency patterns are to be ran.
@@ -93,9 +86,11 @@ val mk_regular :
       a target from certain types of origins, such as generating a tempfile.
  *)
 
+(* TODO? make default ?product=Product.all? *)
+val mk_target_fpath : Analyzer.t -> Fpath.t -> t
+
 (* useful in tests *)
-val mk_target : Analyzer.t -> Fpath.t -> t
-val mk_lang_target : Lang.t -> Fpath.t -> regular
+val mk_lang_target : Lang.t -> Fpath.t -> t
 
 (*****************************************************************************)
 (* Semgrep_output_v1.target -> Target.t *)
@@ -112,8 +107,6 @@ val internal_path : t -> Fpath.t
 
 val origin : t -> Origin.t
 (** [origin target] is the user-reportable origin of [target]. *)
-
-val analyzer : t -> Analyzer.t option
 
 (*
    The set of all lang analyzers associated with targets.

aryx/osemgrep

src/target/Target.ml
@@ -12,7 +12,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
  * LICENSE for more details.
  *)
-open Fpath_.Operators
 module Out = Semgrep_output_v1_j
 
 (*****************************************************************************)
@@ -27,7 +26,7 @@ module Out = Semgrep_output_v1_j
 type path = { origin : Origin.t; internal_path_to_content : Fpath.t }
 [@@deriving show, eq]
 
-type regular = {
+type t = {
   path : path;
   analyzer : Analyzer.t;
   products : Product.t list;
@@ -40,23 +39,14 @@ type regular = {
 }
 [@@deriving show]
 
-type t = Regular of regular | Lockfile of Lockfile.t [@@deriving show]
-
 (*****************************************************************************)
 (* Dumpers *)
 (*****************************************************************************)
 
-let pp_debug_lockfile f (t : Lockfile.t) = Format.fprintf f "%s" !!(t.path)
-
-let pp_debug_regular f (t : regular) =
-  Format.fprintf f "%s (%s)"
+let pp_debug f (t : t) =
+  Format.fprintf f "target file: %s (%s)"
     (t.path.internal_path_to_content |> Fpath.to_string)
     (t.analyzer |> Analyzer.to_string)
-
-let pp_debug f = function
-  | Regular t -> Format.fprintf f "target file: %a" pp_debug_regular t
-  | Lockfile t -> Format.fprintf f "target lockfile: %a" pp_debug_lockfile t
-
 (* needed because of some deriving yojson in Targeting_stat.ml *)
 let to_yojson (x : t) : Yojson.Safe.t =
   let str = show x in
@@ -95,61 +85,52 @@ let path_of_origin (origin : Origin.t) : path =
 (* Builders *)
 (*****************************************************************************)
 
-let mk_regular ?lockfile analyzer products (origin : Origin.t) : regular =
+let mk_target_origin ?lockfile analyzer products (origin : Origin.t) : t =
   { path = path_of_origin origin; analyzer; products; lockfile }
 
 (* useful in test context *)
-let mk_target (analyzer : Analyzer.t) (file : Fpath.t) : t =
+let mk_target_fpath (analyzer : Analyzer.t) (file : Fpath.t) : t =
   let all = Product.all in
   (* TODO: should do the check in the other mk_xxx ? *)
   assert (UFile.is_reg ~follow_symlinks:true file);
-  Regular (mk_regular analyzer all (Origin.File file))
+  mk_target_origin analyzer all (Origin.File file)
 
 (* useful in test context or DeepScan context *)
-let mk_lang_target (lang : Lang.t) (file : Fpath.t) : regular =
-  mk_regular (Analyzer.of_lang lang) Product.all (File file)
+let mk_lang_target (lang : Lang.t) (file : Fpath.t) : t =
+  mk_target_fpath (Analyzer.of_lang lang) file
 
 (*****************************************************************************)
 (* Semgrep_output_v1.target -> Target.t *)
 (*****************************************************************************)
 (* old: used to be Input_to_core.target -> Target.t *)
 
-let code_target_location_of_input_to_core
+let target_location_of_input_to_core
     ({ path; analyzer; products; lockfile_target = lockfile } : Out.code_target)
-    : regular =
-  mk_regular ?lockfile analyzer products (File path)
+    : t =
+  mk_target_origin ?lockfile analyzer products (File path)
 
 let target_of_target (input : Out.target) : t =
   match input with
-  | `CodeTarget x -> Regular (code_target_location_of_input_to_core x)
-  | `LockfileTarget x -> Lockfile x
+  | `CodeTarget x -> target_location_of_input_to_core x
+  | `LockfileTarget _x ->
+      failwith "Impossible: pysemgrep should not generate LockfileTarget here"
 
 (*****************************************************************************)
 (* Accessors *)
 (*****************************************************************************)
 
 let internal_path (target : t) : Fpath.t =
-  match target with
-  | Regular { path = { internal_path_to_content; _ }; _ } ->
-      internal_path_to_content
-  | Lockfile { path; _ } -> path
+  let { path = { internal_path_to_content; _ }; _ } = target in
+  internal_path_to_content
 
 let origin (target : t) : Origin.t =
-  match target with
-  | Regular { path = { origin; _ }; _ } -> origin
-  | Lockfile { path; _ } -> Origin.File path
-
-let analyzer (target : t) : Analyzer.t option =
-  match target with
-  | Regular r -> Some r.analyzer
-  | Lockfile _ -> None
+  let { path = { origin; _ }; _ } = target in
+  origin
 
 let analyzers_of_targets (targets : t list) : Analyzer.t Set_.t =
   List.fold_left
     (fun set target ->
-      match analyzer target with
-      | None -> set
-      | Some a ->
-          let analyzers = Analyzer.flatten a in
-          List_.fold_right Set_.add analyzers set)
+      let a = target.analyzer in
+      let analyzers = Analyzer.flatten a in
+      List_.fold_right Set_.add analyzers set)
     Set_.empty targets

src/target/Target.mli
@@ -32,10 +32,23 @@ type path = {
   } *)
 
 (*****************************************************************************)
-(* Regular target *)
+(* Main type *)
 (*****************************************************************************)
 
-type regular = {
+(*  A semgrep target comprising source code (or, for regex/generic, arbitrary
+    text data) to be executed. This contains all of the details needed to be
+    able to determine how to scan a target, e.g.,
+
+    {ul
+      {- What products should we select rules from?}
+      {- Where can we get the contents of the target?}
+      {- What language should we analyze the target as?}
+    }
+
+    However, it does not contain the actual contents (parsed or otherwise) of
+    the target itself. For that, see {!Xtarget.t}.
+*)
+type t = {
   path : path;
   analyzer : Analyzer.t;  (** The analyzer to use when scanning this target. *)
   products : Product.t list;
@@ -48,37 +61,18 @@ type regular = {
           specified by the given lockfile. Core doesn't need to worry about
           determining these associations; rather, the target selection and
           generation process must resolve these connections as part of
-          generating regular targets. *)
+          generating targets. *)
 }
-(** A regular semgrep target, comprising source code (or, for
-   regex/generic, arbitrary text data) to be executed. See also {!Xtarget.t},
-   an augmented version which also has the contents. *)
-
-(*****************************************************************************)
-(* Main type *)
-(*****************************************************************************)
-
-(** A Semgrep target. This contains all of the details needed to be able to
-    determine how to scan a target, e.g.,
-
-    {ul
-      {- What products should we select rules from?}
-      {- Where can we get the contents of the target?}
-      {- What language should we analyze the target as?}
-    }
-
-    However, it does not contain the actual contents (parsed or otherwise) of
-    the target itself. For that, see {!Xtarget.t} or {!Lockfile_xtarget}.
- *)
-type t = Regular of regular | Lockfile of Lockfile.t [@@deriving show]
+[@@deriving show]
 
 (*****************************************************************************)
 (* Builders *)
 (*****************************************************************************)
 
-val mk_regular :
-  ?lockfile:Lockfile.t -> Analyzer.t -> Product.t list -> Origin.t -> regular
-(** [mk_regular analyzer products origin] is a {!regular} target
+(* TODO? make default ?product=Product.all? *)
+val mk_target_origin :
+  ?lockfile:Lockfile.t -> Analyzer.t -> Product.t list -> Origin.t -> t
+(** [mk_target_origin analyzer products origin] is a target
       originating from [origin] to be analyzed with [analyzer] for [products].
       If [lockfile] is specified then it shall be used as the associated
       lockfile if dependency patterns are to be ran.
@@ -88,9 +82,11 @@ val mk_regular :
       a target from certain types of origins, such as generating a tempfile.
  *)
 
+(* TODO? make default ?product=Product.all? *)
+val mk_target_fpath : Analyzer.t -> Fpath.t -> t
+
 (* useful in tests *)
-val mk_target : Analyzer.t -> Fpath.t -> t
-val mk_lang_target : Lang.t -> Fpath.t -> regular
+val mk_lang_target : Lang.t -> Fpath.t -> t
 
 (*****************************************************************************)
 (* Semgrep_output_v1.target -> Target.t *)
@@ -107,8 +103,6 @@ val internal_path : t -> Fpath.t
 
 val origin : t -> Origin.t
 (** [origin target] is the user-reportable origin of [target]. *)
-
-val analyzer : t -> Analyzer.t option
 
 (*
    The set of all lang analyzers associated with targets.